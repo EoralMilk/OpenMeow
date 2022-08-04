penev92/OpenRA

Add a hierarchical path finder to improve pathfinding performance.
Replaces the existing bi-directional search between points used by the pathfinder with a guided hierarchical search. The old search was a standard A* search with a heuristic of advancing in straight line towards the target. This heuristic performs well if a mostly direct path to the target exists, it performs poorly it the path has to navigate around blockages in the terrain. The hierarchical path finder maintains a simplified, abstract graph. When a path search is performed it uses this abstract graph to inform the heuristic. Instead of moving blindly towards the target, it will instead steer around major obstacles, almost as if it had been provided a map which ensures it can move in roughly the right direction. This allows it to explore less of the area overall, improving performance.

When a path needs to steer around terrain on the map, the hierarchical path finder is able to greatly improve on the previous performance. When a path is able to proceed in a straight line, no performance benefit will be seen. If the path needs to steer around actors on the map instead of terrain (e.g. trees, buildings, units) then the same poor pathfinding performance as before will be observed.

OpenRA.Mods.Common/Pathfinder/GridPathGraph.cs
@@ -1,0 +1,54 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.Common.Pathfinder
+{
+	/// <summary>
+	/// A dense pathfinding graph that supports a search over all cells within a <see cref="Grid"/>.
+	/// Cells outside the grid area are deemed unreachable and will not be considered.
+	/// It implements the ability to cost and get connections for cells, and supports <see cref="ICustomMovementLayer"/>.
+	/// </summary>
+	sealed class GridPathGraph : DensePathGraph
+	{
+		readonly CellInfo[] infos;
+		readonly Grid grid;
+
+		public GridPathGraph(Locomotor locomotor, Actor actor, World world, BlockedByActor check,
+			Func<CPos, int> customCost, Actor ignoreActor, bool laneBias, bool inReverse, Grid grid)
+			: base(locomotor, actor, world, check, customCost, ignoreActor, laneBias, inReverse)
+		{
+			infos = new CellInfo[grid.Width * grid.Height];
+			this.grid = grid;
+		}
+
+		protected override bool NeighborAllowable(CPos neighbor)
+		{
+			// Enforce that we only search within the grid bounds.
+			return grid.Contains(neighbor);
+		}
+
+		int InfoIndex(CPos pos)
+		{
+			return
+				(pos.Y - grid.TopLeft.Y) * grid.Width +
+				(pos.X - grid.TopLeft.X);
+		}
+
+		public override CellInfo this[CPos pos]
+		{
+			get => infos[InfoIndex(pos)];
+			set => infos[InfoIndex(pos)] = value;
+		}
+	}
+}

EoralMilk/OpenMeow

OpenRA.Mods.Common/Pathfinder/GridPathGraph.cs
@@ -1,0 +1,54 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.Common.Pathfinder
+{
+	/// <summary>
+	/// A dense pathfinding graph that supports a search over all cells within a <see cref="Grid"/>.
+	/// Cells outside the grid area are deemed unreachable and will not be considered.
+	/// It implements the ability to cost and get connections for cells, and supports <see cref="ICustomMovementLayer"/>.
+	/// </summary>
+	sealed class GridPathGraph : DensePathGraph
+	{
+		readonly CellInfo[] infos;
+		readonly Grid grid;
+
+		public GridPathGraph(Locomotor locomotor, Actor actor, World world, BlockedByActor check,
+			Func<CPos, int> customCost, Actor ignoreActor, bool laneBias, bool inReverse, Grid grid)
+			: base(locomotor, actor, world, check, customCost, ignoreActor, laneBias, inReverse)
+		{
+			infos = new CellInfo[grid.Width * grid.Height];
+			this.grid = grid;
+		}
+
+		protected override bool IsValidNeighbor(CPos neighbor)
+		{
+			// Enforce that we only search within the grid bounds.
+			return grid.Contains(neighbor);
+		}
+
+		int InfoIndex(CPos pos)
+		{
+			return
+				(pos.Y - grid.TopLeft.Y) * grid.Width +
+				(pos.X - grid.TopLeft.X);
+		}
+
+		public override CellInfo this[CPos pos]
+		{
+			get => infos[InfoIndex(pos)];
+			set => infos[InfoIndex(pos)] = value;
+		}
+	}
+}