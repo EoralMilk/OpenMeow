penev92/OpenRA

Replace DomainIndex internals with a lookup from HierarchicalPathFinder instead
Teach HierarchicalPathFinder to keep a cache of domain indices, refreshing them only on demand and when invalidated by terrain changes. This provides an accurate and quick determination for checking if paths exist between given locations.

By exposing PathExistsForLocomotor on the IPathFinder interface, we can remove the DomainIndex trait entirely.

OpenRA.Mods.Common/Traits/BotModules/Squads/States/NavyStates.cs
@@ -28,12 +28,11 @@ namespace OpenRA.Mods.Common.Traits.BotModules.Squads
 			// Navy squad AI can exploit enemy naval production to find path, if any.
 			// (Way better than finding a nearest target which is likely to be on Ground)
 			// You might be tempted to move these lookups into Activate() but that causes null reference exception.
-			var domainIndex = first.World.WorldActor.Trait<DomainIndex>();
-			var locomotor = first.Trait<Mobile>().Locomotor;
+			var mobile = first.Trait<Mobile>();
 
 			var navalProductions = owner.World.ActorsHavingTrait<Building>().Where(a
 				=> owner.SquadManager.Info.NavalProductionTypes.Contains(a.Info.Name)
-				&& domainIndex.IsPassable(first.Location, a.Location, locomotor)
+				&& mobile.PathFinder.PathExistsForLocomotor(mobile.Locomotor, first.Location, a.Location)
 				&& a.AppearsHostileTo(first));
 
 			if (navalProductions.Any())

EoralMilk/OpenMeow

OpenRA.Mods.Common/Traits/BotModules/Squads/States/NavyStates.cs
@@ -22,12 +22,11 @@ namespace OpenRA.Mods.Common.Traits.BotModules.Squads
 			// Navy squad AI can exploit enemy naval production to find path, if any.
 			// (Way better than finding a nearest target which is likely to be on Ground)
 			// You might be tempted to move these lookups into Activate() but that causes null reference exception.
-			var domainIndex = leader.World.WorldActor.Trait<DomainIndex>();
-			var locomotor = leader.Trait<Mobile>().Locomotor;
+			var mobile = leader.Trait<Mobile>();
 
 			var navalProductions = owner.World.ActorsHavingTrait<Building>().Where(a
 				=> owner.SquadManager.Info.NavalProductionTypes.Contains(a.Info.Name)
-				&& domainIndex.IsPassable(leader.Location, a.Location, locomotor)
+				&& mobile.PathFinder.PathExistsForLocomotor(mobile.Locomotor, leader.Location, a.Location)
 				&& a.AppearsHostileTo(leader));
 
 			if (navalProductions.Any())