b9038fa204da5349c91340c3f2380e90c79283b1
abcdefg30 <alphabet@streber24.de>
2022-10-13 22:47:53 +0200

    Remove the filename from the undefined MapFormat exception

--- OpenRA.Game/Map/Map.cs
+++ OpenRA.Game/Map/Map.cs
@@ -330,7 +330,7 @@ namespace OpenRA
 					return FieldLoader.GetValue<int>("MapFormat", search.Groups[1].Value);
 			}
 
-			throw new InvalidDataException($"MapFormat is not defined\n File: {p.Name}");
+			throw new InvalidDataException("MapFormat is not defined");
 		}
 
 		/// <summary>
