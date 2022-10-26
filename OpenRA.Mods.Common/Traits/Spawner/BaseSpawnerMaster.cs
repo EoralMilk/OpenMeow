6004f892b22e3a821e8ce058c373768d22a1b8b6
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2022-10-26 22:17:37 +0300

    Fix crash with Spawned Facing.

--- OpenRA.Mods.Common/Traits/Spawner/BaseSpawnerMaster.cs
+++ OpenRA.Mods.Common/Traits/Spawner/BaseSpawnerMaster.cs
@@ -243,11 +243,13 @@ namespace OpenRA.Mods.Common.Traits
 		{
 			var facingOffset = facing == null ? WAngle.Zero : facing.Facing;
 
-			var exitFacing = exit != null ? exit.Info.Facing : WAngle.Zero;
+			var exitFacing = WAngle.Zero;
+			if (exit != null && exit.Info.Facing.HasValue)
+				exitFacing = exit.Info.Facing.Value;
 
 			var spawnFacing = spawned.TraitOrDefault<IFacing>();
 			if (spawnFacing != null)
-				spawnFacing.Facing = facingOffset + exitFacing.Value;
+				spawnFacing.Facing = facingOffset + exitFacing;
 		}
 
 		public void StopSlaves()
