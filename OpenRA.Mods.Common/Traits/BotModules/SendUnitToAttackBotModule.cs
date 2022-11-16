e214a4d9fd38d39f4900d5167578f0659db211c2
dnqbob <dnqbob@outlook.com>
2022-11-16 22:43:01 +0800

    钻地兵和SendUnitToAttackBotModule的崩溃修复

--- OpenRA.Mods.Common/Traits/BotModules/SendUnitToAttackBotModule.cs
+++ OpenRA.Mods.Common/Traits/BotModules/SendUnitToAttackBotModule.cs
@@ -145,7 +145,11 @@ namespace OpenRA.Mods.Common.Traits
 					return;
 
 				// Randomly choose enemy player to attack
-				targetPlayer = world.Players.Where(p => p.RelationshipWith(player) == PlayerRelationship.Enemy).Random(world.LocalRandom);
+				var enemyPlayers = world.Players.Where(p => p.RelationshipWith(player) == PlayerRelationship.Enemy && p.WinState != WinState.Lost).ToArray();
+				if (enemyPlayers.Length == 0)
+					return;
+
+				targetPlayer = enemyPlayers.Where(p => p.RelationshipWith(player) == PlayerRelationship.Enemy).Random(world.LocalRandom);
 
 				var targets = world.Actors.Where(a =>
 				{
